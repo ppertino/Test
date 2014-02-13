71d4d4630c8c75d935072861252ae9c34c1b643f
PPertino <ppertino@remotingcoders.com>
2014-02-13 16:39:59 -0300

    ASD

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word");
+            Console.WriteLine("Hello Word");
         }
     }
 }
